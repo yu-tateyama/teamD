866ad8c8f83c4561b1c2eb17688aa2f5da7e6c86
tak-yoshikawa <tak-yoshikawa@brexa.com>
2026-06-09 09:12:11 +0900

    Create dump.sh for PostgreSQL database backup (AMI)
    Add a PostgreSQL dump script with timing and logging.

--- dump.sh
+++ dump.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+start_time=$(date +%s)
+echo $start_time > /usr/local/dump/dump.log
+set PGPASSWORD=Zaq12wsx
+/usr/local/postgresql/bin/pg_dump --dbname=postgresql://postgres:Zaq12wsx@localhost:5432/dash_replace -F c -f /tmp/hr_dash_`date +%Y%m%d`.dmp
+#/usr/local/postgresql/bin/pg_dump --dbname=postgresql://postgres:Zaq12wsx@localhost:dash_replace -U postgres -h 127.0.0.1 dash_replace -F c -f /tmp/hr_dash_`date +%Y%m%d`.dmp
+end_time=$(date +%s)
+echo $end_time > /usr/local/dump/dump.log
+elapsed_time=$((end_time - start_time))
+echo "dumpにかかった時間: $elapsed_time 秒" > /usr/local/dump/dump.log
